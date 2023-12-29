9a676d128e779429b92ace6edfc47a45799cf0a4
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-12-29 13:15:08 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenHongQuan489.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
