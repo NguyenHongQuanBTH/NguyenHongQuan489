5d57ab8534eb3a26f8cfc7422bd2ba5d48331174
NguyenHongQuanBTH <quanto190301@gmail.com>
2024-01-06 12:19:24 +0700

    first

---
+++ Data/ApplicationDbcontext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NguyenHongQuan489.Models;
+namespace NguyenHongQuan489.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+    public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+    {}
+    public DbSet<Person> Person { get; set; }
+
+    }
+}
